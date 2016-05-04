c1327d76166a5e1656abd88bfe0bb3b12d253fde
Khas195 <gamer1091995@yahoo.com>
2016-05-04 13:50:41 +0700

    fix StateStack:
    + use shared Pointer
    + add getTop()
    + add isEmpty();

--- WaterMelonEngine/StateStack.h
+++ WaterMelonEngine/StateStack.h
@@ -1,15 +1,21 @@
 #pragma once
 #include <stack>
+#include <assert.h>
+#include <memory>
+
 #include "IState.h"
 
 class StateStack
 {
-	std::stack<IState*> states;
+	std::stack<std::shared_ptr<IState>> states;
 public:
 	StateStack();
 	~StateStack();
 
-	void push(IState* state);
-	IState* pop();
+	void push(std::shared_ptr<IState> state);
+	bool isEmpty();
+	std::shared_ptr<IState> pop();
+	std::shared_ptr<IState> getTop();
+
 };
 
