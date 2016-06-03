cbb1a37bd771a5a6599e516bc478190e43e86d2e
Khas195 <gamer1091995@yahoo.com>
2016-06-03 12:56:51 +0700

    asd

--- WaterMelonEngine/ActionButton.cpp
+++ WaterMelonEngine/ActionButton.cpp
@@ -1,27 +1,43 @@
 #include "ActionButton.h"
+#include "WaterMelonEngine.h"
 
-void ActionButton::setPosition(sf::Vector2f & pos)
+bool ActionButton::checkBetween(int begin, int end, int target) const
 {
-	action_button_menu.setPosition(pos);
+	if (begin <= target && target <= end)
+		return true;
+	return false;
 }
-
-void ActionButton::setPosition(float x, float y)
+ActionButton::ActionButton(Sprite buttonSprite, sf::Vector2f clickedFramePos, sf::Vector2f normalFramePos)
+	: buttonSprite(buttonSprite), clickedFrame(clickedFramePos), normalFrame(normalFramePos), hoverFrame(&normalFrame)
+{}
+void ActionButton::setHoverFrame(sf::Vector2f * hoverFrame)
 {
-	action_button_menu.setPosition(x, y);
+	this->hoverFrame = hoverFrame;
 }
-
-const sf::Vector2f & ActionButton::getSize()
-{
-}
-
 void ActionButton::update(sf::Event::EventType & type)
 {
-}
+	sf::Vector2i mousePos = sf::Mouse::getPosition(WaterMelonEngine::getWindow());
+	int buttonX = buttonSprite.getPosition().x;
+	int buttonY = buttonSprite.getPosition().y;
 
+	if (checkBetween(buttonX, buttonX + buttonSprite.getLocalSize().x, mousePos.x)
+		&& checkBetween(buttonY, buttonY + buttonSprite.getLocalSize().y, mousePos.y))
+	{
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			buttonSprite.setCurrentFrame(clickedFrame.x, clickedFrame.y);
+		}
+		buttonSprite.setCurrentFrame(hoverFrame->x, hoverFrame->y);
+	}
+	else buttonSprite.setCurrentFrame(normalFrame.x, normalFrame.y);
+}
 void ActionButton::render(sf::RenderWindow & window)
 {
+	buttonSprite.render(window);
 }
-
+void ActionButton::receiveMessage(Package * package)
+{
+}
 std::string ActionButton::getName()
 {
 	return std::string();
